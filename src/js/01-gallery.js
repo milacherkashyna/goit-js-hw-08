f229369e6a4c9451a4d34223d1b4dea3a8addac5
milacherkashyna <mila.cherkashyna@gmail.com>
2023-02-15 15:26:32 +0200

    secomd commite

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const markUp = galleryItems
+  .map(
+    item => `
+    <a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+  </a>
+`
+  )
+  .join('');
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML(`beforeend`, markUp);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+// Change code below this line
